b6b28750409c1c54b6a194510389f1eca45efd60
Michael Bird <michaelbirdtx@protonmail.com>
2022-05-29 19:43:32 -0500

    Added level 80 gear, plus refinements.

--- levelUp.lua
+++ levelUp.lua
@@ -83,6 +83,19 @@ local Gear = {
     {class = 1024, level = 60, item = 6, entry = 16829},
     {class = 1024, level = 60, item = 7, entry = 16830},
 
+    {class = 1024, level = 80, item =  1, entry = 43261},
+    {class = 1024, level = 80, item =  2, entry = 43262},
+    {class = 1024, level = 80, item =  3, entry = 43263},
+    {class = 1024, level = 80, item =  4, entry = 43271},
+    {class = 1024, level = 80, item =  5, entry = 43266},
+    {class = 1024, level = 80, item =  6, entry = 43265},
+    {class = 1024, level = 80, item =  7, entry = 43273},
+    {class = 1024, level = 80, item =  8, entry = 43264},
+    {class = 1024, level = 80, item =  9, entry = 45810},
+    {class = 1024, level = 80, item = 10, entry = 37384},
+    {class = 1024, level = 80, item = 11, entry = 37190},
+    {class = 1024, level = 80, item = 12, entry = 32387},
+
     -- Hunter
     {class = 4, level = 20, item = 1, entry = 30419},
     {class = 4, level = 20, item = 2, entry = 5193},
@@ -107,6 +120,19 @@ local Gear = {
     {class = 4, level = 60, item = 5, entry = 16852},
     {class = 4, level = 60, item = 6, entry = 16849},
     {class = 4, level = 60, item = 7, entry = 16850},
+
+    {class = 4, level = 80, item =  1, entry = 43447},
+    {class = 4, level = 80, item =  2, entry = 43449},
+    {class = 4, level = 80, item =  3, entry = 43445},
+    {class = 4, level = 80, item =  4, entry = 43448},
+    {class = 4, level = 80, item =  5, entry = 43442},
+    {class = 4, level = 80, item =  6, entry = 43446},
+    {class = 4, level = 80, item =  7, entry = 43443},
+    {class = 4, level = 80, item =  8, entry = 43444},
+    {class = 4, level = 80, item =  9, entry = 38441},
+    {class = 4, level = 80, item = 10, entry = 37615},
+    {class = 4, level = 80, item = 11, entry = 36962},
+    {class = 4, level = 80, item = 12, entry = 41167},
 
     -- Mage
     {class = 128, level = 20, item = 1, entry = 30419},
@@ -133,6 +159,19 @@ local Gear = {
     {class = 128, level = 60, item = 6, entry = 16800},
     {class = 128, level = 60, item = 7, entry = 16799},
 
+    {class = 128, level = 80, item =  1, entry = 43971},
+    {class = 128, level = 80, item =  2, entry = 43973},
+    {class = 128, level = 80, item =  3, entry = 43972},
+    {class = 128, level = 80, item =  4, entry = 43975},
+    {class = 128, level = 80, item =  5, entry = 43969},
+    {class = 128, level = 80, item =  6, entry = 41516},
+    {class = 128, level = 80, item =  7, entry = 43970},
+    {class = 128, level = 80, item =  8, entry = 43974},
+    {class = 128, level = 80, item =  9, entry = 45810},
+    {class = 128, level = 80, item = 10, entry = 37384},
+    {class = 128, level = 80, item = 11, entry = 37626},
+    {class = 128, level = 80, item = 12, entry = 37177},
+
     -- Paladin
     {class = 2, level = 20, item = 1, entry = 30419},
     {class = 2, level = 20, item = 2, entry = 20428},
@@ -157,6 +196,19 @@ local Gear = {
     {class = 2, level = 60, item = 5, entry = 16860},
     {class = 2, level = 60, item = 6, entry = 16859},
     {class = 2, level = 60, item = 7, entry = 16857},
+
+    {class = 2, level = 80, item =  1, entry = 42728},
+    {class = 2, level = 80, item =  2, entry = 42727},
+    {class = 2, level = 80, item =  3, entry = 42725},
+    {class = 2, level = 80, item =  4, entry = 42726},
+    {class = 2, level = 80, item =  5, entry = 42729},
+    {class = 2, level = 80, item =  6, entry = 42724},
+    {class = 2, level = 80, item =  7, entry = 42730},
+    {class = 2, level = 80, item =  8, entry = 42723},
+    {class = 2, level = 80, item =  9, entry = 45810},
+    {class = 2, level = 80, item = 10, entry = 41188},
+    {class = 2, level = 80, item = 11, entry = 42443},
+    {class = 2, level = 80, item = 12, entry = 41117},
 
     -- Priest
     {class = 16, level = 20, item = 1, entry = 30419},
@@ -183,6 +235,19 @@ local Gear = {
     {class = 16, level = 60, item = 6, entry = 16811},
     {class = 16, level = 60, item = 7, entry = 16819},
 
+    {class = 16, level = 80, item =  1, entry = 43971},
+    {class = 16, level = 80, item =  2, entry = 43973},
+    {class = 16, level = 80, item =  3, entry = 43972},
+    {class = 16, level = 80, item =  4, entry = 43975},
+    {class = 16, level = 80, item =  5, entry = 43969},
+    {class = 16, level = 80, item =  6, entry = 41516},
+    {class = 16, level = 80, item =  7, entry = 43970},
+    {class = 16, level = 80, item =  8, entry = 43974},
+    {class = 16, level = 80, item =  9, entry = 45810},
+    {class = 16, level = 80, item = 10, entry = 37384},
+    {class = 16, level = 80, item = 11, entry = 37626},
+    {class = 16, level = 80, item = 12, entry = 37177},
+
     -- Rogue
     {class = 8, level = 20, item = 1, entry = 30419},
     {class = 8, level = 20, item = 2, entry = 5193},
@@ -207,6 +272,19 @@ local Gear = {
     {class = 8, level = 60, item = 5, entry = 16826},
     {class = 8, level = 60, item = 6, entry = 16824},
     {class = 8, level = 60, item = 7, entry = 16825},
+
+    {class = 8, level = 80, item =  1, entry = 43260},
+    {class = 8, level = 80, item =  2, entry = 43433},
+    {class = 8, level = 80, item =  3, entry = 43434},
+    {class = 8, level = 80, item =  4, entry = 43438},
+    {class = 8, level = 80, item =  5, entry = 43437},
+    {class = 8, level = 80, item =  6, entry = 43436},
+    {class = 8, level = 80, item =  7, entry = 43439},
+    {class = 8, level = 80, item =  8, entry = 43435},
+    {class = 8, level = 80, item =  9, entry = 38441},
+    {class = 8, level = 80, item = 10, entry = 41184},
+    {class = 8, level = 80, item = 11, entry = 41184},
+    {class = 8, level = 80, item = 12, entry = 41245},
 
     -- Shaman
     {class = 64, level = 20, item = 1, entry = 30419},
@@ -233,6 +311,19 @@ local Gear = {
     {class = 64, level = 60, item = 6, entry = 16837},
     {class = 64, level = 60, item = 7, entry = 16840},
 
+    {class = 64, level = 80, item =  1, entry = 43455},
+    {class = 64, level = 80, item =  2, entry = 43457},
+    {class = 64, level = 80, item =  3, entry = 43453},
+    {class = 64, level = 80, item =  4, entry = 43456},
+    {class = 64, level = 80, item =  5, entry = 43450},
+    {class = 64, level = 80, item =  6, entry = 43454},
+    {class = 64, level = 80, item =  7, entry = 43451},
+    {class = 64, level = 80, item =  8, entry = 43452},
+    {class = 64, level = 80, item =  9, entry = 45810},
+    {class = 64, level = 80, item = 10, entry = 41188},
+    {class = 64, level = 80, item = 11, entry = 42443},
+    {class = 64, level = 80, item = 12, entry = 41117},
+
     -- Warlock
     {class = 256, level = 20, item = 1, entry = 30419},
     {class = 256, level = 20, item = 2, entry = 12979},
@@ -258,6 +349,19 @@ local Gear = {
     {class = 256, level = 60, item = 6, entry = 16803},
     {class = 256, level = 60, item = 7, entry = 16804},
 
+    {class = 256, level = 80, item =  1, entry = 43971},
+    {class = 256, level = 80, item =  2, entry = 43973},
+    {class = 256, level = 80, item =  3, entry = 43972},
+    {class = 256, level = 80, item =  4, entry = 43975},
+    {class = 256, level = 80, item =  5, entry = 43969},
+    {class = 256, level = 80, item =  6, entry = 41516},
+    {class = 256, level = 80, item =  7, entry = 43970},
+    {class = 256, level = 80, item =  8, entry = 43974},
+    {class = 256, level = 80, item =  9, entry = 45810},
+    {class = 256, level = 80, item = 10, entry = 37384},
+    {class = 256, level = 80, item = 11, entry = 37626},
+    {class = 256, level = 80, item = 12, entry = 37177},
+
     -- Warrior
     {class = 1, level = 20, item = 1, entry = 30419},
     {class = 1, level = 20, item = 2, entry = 5193},
@@ -282,6 +386,19 @@ local Gear = {
     {class = 1, level = 60, item = 5, entry = 16863},
     {class = 1, level = 60, item = 6, entry = 16862},
     {class = 1, level = 60, item = 7, entry = 16861},
+
+    {class = 1, level = 80, item =  1, entry = 41350},
+    {class = 1, level = 80, item =  2, entry = 41351},
+    {class = 1, level = 80, item =  3, entry = 41353},
+    {class = 1, level = 80, item =  4, entry = 41347},
+    {class = 1, level = 80, item =  5, entry = 41352},
+    {class = 1, level = 80, item =  6, entry = 41349},
+    {class = 1, level = 80, item =  7, entry = 41348},
+    {class = 1, level = 80, item =  8, entry = 41354},
+    {class = 1, level = 80, item =  9, entry = 45811},
+    {class = 1, level = 80, item = 10, entry = 41186},
+    {class = 1, level = 80, item = 11, entry = 41186},
+    {class = 1, level = 80, item = 12, entry = 41113},
 }
 
 function findGear(class, level, item)
@@ -295,6 +412,11 @@ end
 function levelCheck(event, player, oldLevel)
     class = player:GetClassMask()
     level = player:GetLevel()
+    emailSubject = "Congrats on "..level.."!"
+    emailBody = "Congratulations on reaching level "..level..", "..player:GetName().."! Here are some tokens of our appreciation!"
+    emailTo = player:GetGUIDLow()
+    emailStationery = 61
+    emailFrom = 0
     if (level==20 or level==40 or level==60) and class ~= 32 then
         print("["..MODULE_NAME.."]: Level: "..level.." Class: "..class)
         player:SendNotification("Congrats on Level "..level.."!")
@@ -303,13 +425,14 @@ function levelCheck(event, player, oldLevel)
         randomTitle = math.random(1,#Titles)
         player:SetKnownTitle(Titles[randomTitle])
         SendMail(
-            "Congrats!", "Congratulations on reaching level "..level..", "..player:GetName().."! Here are some tokens of our appreciation!",
-            player:GetGUIDLow(),
-            0,
-            61,
-            0,
+            emailSubject,
+            emailBody,
+            emailTo,
+            emailFrom,
+            emailStationery,
+            0, -- Delay
             Gold[level],
-            0,
+            0, -- COD
             Pets[randomPet],
             1,
             findGear(class,level,1),
@@ -325,6 +448,48 @@ function levelCheck(event, player, oldLevel)
             findGear(class,level,6),
             1,
             findGear(class,level,7),
+            1
+        )
+    end
+    if level==80 then
+        print("["..MODULE_NAME.."]: Level: "..level.." Class: "..class)
+        player:SendNotification("Congrats on Level "..level.."!")
+        player:PlayDirectSound(8572)
+        player:SetKnownTitle(142) -- the Undying
+        SendMail(
+            emailSubject,
+            emailBody,
+            emailTo,
+            emailFrom,
+            emailStationery,
+            0, -- Delay
+            Gold[level],
+            0, -- COD
+            56806, -- Mini Thor
+            1,
+            findGear(class,level,1),
+            1,
+            findGear(class,level,2),
+            1,
+            findGear(class,level,3),
+            1,
+            findGear(class,level,4),
+            1,
+            findGear(class,level,5),
+            1,
+            findGear(class,level,6),
+            1,
+            findGear(class,level,7),
+            1,
+            findGear(class,level,8),
+            1,
+            findGear(class,level,9),
+            1,
+            findGear(class,level,10),
+            1,
+            findGear(class,level,11),
+            1,
+            findGear(class,level,12),
             1
         )
     end
