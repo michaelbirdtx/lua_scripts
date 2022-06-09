00cb3eca8e0322ae634895c79ab70b94a4ecf90f
Michael Bird <michaelbirdtx@protonmail.com>
2022-06-09 04:00:29 -0500

    Changed functions to local. Added mounts. Changed Class from mask to ID.

--- newToon.lua
+++ newToon.lua
@@ -7,13 +7,13 @@ print("["..MODULE_NAME.."]: Loaded, Version "..MODULE_VERSION.." Active")
 local Skills = {
 
     -- Warrior (1)
-    {class = 1, entry = 264},   -- Bows
-    {class = 1, entry = 5011},  -- Crossbow
+    {class = 1, entry =   264}, -- Bows
+    {class = 1, entry =  5011}, -- Crossbow
     {class = 1, entry = 15590}, -- Fists
-    {class = 1, entry = 266},   -- Guns
-    {class = 1, entry = 200},   -- Polearms
-    {class = 1, entry = 199},   -- 2H Mace
-    {class = 1, entry = 227},   -- Staves
+    {class = 1, entry =   266}, -- Guns
+    {class = 1, entry =   200}, -- Polearms
+    {class = 1, entry =   199}, -- 2H Mace
+    {class = 1, entry =   227}, -- Staves
 
     -- Paladin (2)
     {class = 2, entry = 196}, -- Axes
@@ -21,46 +21,56 @@ local Skills = {
     {class = 2, entry = 197}, -- 2H Axe
     {class = 2, entry = 199}, -- 2H Mace
 
-    -- Hunter (4)
-    {class = 4, entry = 15590}, -- Fists
+    -- Hunter (3)
+    {class = 3, entry = 15590}, -- Fists
+    {class = 3, entry =   264}, -- Bows
+    {class = 3, entry =   266}, -- Guns
+    {class = 3, entry =   200}, -- Polearms
+    {class = 3, entry =   227}, -- Staves
+    {class = 3, entry =   202}, -- 2H Swords
+
+    -- Rogue (4)
     {class = 4, entry = 264},   -- Bows
+    {class = 4, entry = 5011},  -- Crossbow
+    {class = 4, entry = 15590}, -- Fists
     {class = 4, entry = 266},   -- Guns
-    {class = 4, entry = 200},   -- Polearms
-    {class = 4, entry = 227},   -- Staves
-    {class = 4, entry = 202},   -- 2H Swords
+    {class = 4, entry = 196},   -- Axes
+    {class = 4, entry = 198},   -- Maces
+    {class = 4, entry = 201},   -- Swords
 
-    -- Rogue (8)
-    {class = 8, entry = 264},   -- Bows
-    {class = 8, entry = 5011},  -- Crossbow
-    {class = 8, entry = 15590}, -- Fists
-    {class = 8, entry = 266},   -- Guns
-    {class = 8, entry = 196},   -- Axes
-    {class = 8, entry = 198},   -- Maces
-    {class = 8, entry = 201},   -- Swords
+    -- Priest (5)
+    {class = 5, entry = 1180}, -- Daggers
 
-    -- Priest (16)
-    {class = 16, entry = 1180}, -- Daggers
+    -- Shaman (7)
+    {class = 7, entry = 15590}, -- Fists
+    {class = 7, entry =   196}, -- Axes
+    {class = 7, entry =   197}, -- 2H Axe
+    {class = 7, entry =   199}, -- 2H Mace
 
-    -- Shaman (64)
-    {class = 64, entry = 15590}, -- Fists
-    {class = 64, entry = 196},   -- Axes
-    {class = 64, entry = 197},   -- 2H Axe
-    {class = 64, entry = 199},   -- 2H Mace
+    -- Mage (8)
+    {class = 8, entry = 201}, -- Swords
 
-    -- Mage (128)
-    {class = 128, entry = 201}, -- Swords
+    -- Warlock (9)
+    {class = 9, entry =   201}, -- Swords
+    {class = 9, entry =  5784}, -- Felsteed
+    {class = 9, entry = 23161}, -- Dreadsteed
 
-    -- Warlock (256)
-    {class = 256, entry = 201}, -- Swords
+    -- Druid (11)
+    {class = 11, entry =  1180}, -- Daggers
+    {class = 11, entry = 15590}, -- Fists
+    {class = 11, entry =   198}, -- Guns
+    {class = 11, entry =   200}, -- Axes
+    {class = 11, entry =   227}, -- Maces
+    {class = 11, entry =   199}, -- Swords
 
-    -- Druid (1024)
-    {class = 1024, entry = 1180},  -- Daggers
-    {class = 1024, entry = 15590}, -- Fists
-    {class = 1024, entry = 198},   -- Guns
-    {class = 1024, entry = 200},   -- Axes
-    {class = 1024, entry = 227},   -- Maces
-    {class = 1024, entry = 199},   -- Swords
+}
 
+local Mounts = {
+    {race =  1, entry = 63232}, -- (Human) Stormwind Steed
+    {race =  3, entry = 63636}, -- (Dwarf) Ironforge Ram
+    {race =  4, entry = 63637}, -- (Night Elf) Darnassian Nightsaber
+    {race =  7, entry = 63638}, -- (Gnome) Gnomeregan Mechanostrider
+    {race = 11, entry = 63639}, -- (Draenei) Exodar Elekk
 }
 
 local Gear = {
@@ -95,114 +105,113 @@ local Gear = {
     {class = 2, item = 12, entry = 42991}, -- Swift Hand of Justice
     {class = 2, item = 13, entry = 42992}, -- Discerning Eye of the Beast
 
-    -- Hunter (4)
-    {class = 4, item =  1, entry = 42950}, -- Champion Herod's Shoulder
-    {class = 4, item =  2, entry = 48677}, -- Champion's Deathdealer Breastplate
+    -- Hunter (3)
+    {class = 3, item =  1, entry = 42950}, -- Champion Herod's Shoulder
+    {class = 3, item =  2, entry = 48677}, -- Champion's Deathdealer Breastplate
+    {class = 3, item =  3, entry =  2126}, -- Cracked Leather Pants
+    {class = 3, item =  4, entry =  2122}, -- Cracked Leather Belt
+    {class = 3, item =  5, entry =  2125}, -- Cracked Leather Gloves
+    {class = 3, item =  6, entry =  2123}, -- Cracked Leather Boots
+    {class = 3, item =  7, entry =  2124}, -- Cracked Leather Bracers
+    {class = 3, item =  8, entry = 14385}, -- Durability Cloak
+    {class = 3, item =  9, entry = 42943}, -- Bloodied Arcanite Reaper
+    {class = 3, item = 10, entry = 42946}, -- Charmed Ancient Bone Bow
+    {class = 3, item = 11, entry = 50255}, -- Dread Pirate Ring
+    {class = 3, item = 12, entry = 42991}, -- Swift Hand of Justice
+    {class = 3, item = 13, entry = 42992}, -- Discerning Eye of the Beast
+
+    -- Rogue (4)
+    {class = 4, item =  1, entry = 42952}, -- Stained Shadowcraft Shoulders
+    {class = 4, item =  2, entry = 48689}, -- Stained Shadowcraft Tunic
     {class = 4, item =  3, entry =  2126}, -- Cracked Leather Pants
     {class = 4, item =  4, entry =  2122}, -- Cracked Leather Belt
     {class = 4, item =  5, entry =  2125}, -- Cracked Leather Gloves
     {class = 4, item =  6, entry =  2123}, -- Cracked Leather Boots
     {class = 4, item =  7, entry =  2124}, -- Cracked Leather Bracers
     {class = 4, item =  8, entry = 14385}, -- Durability Cloak
-    {class = 4, item =  9, entry = 42943}, -- Bloodied Arcanite Reaper
-    {class = 4, item = 10, entry = 42946}, -- Charmed Ancient Bone Bow
+    {class = 4, item =  9, entry = 42944}, -- Balanced Heartseeker
+    {class = 4, item = 10, entry = 42944}, -- Balanced Heartseeker
     {class = 4, item = 11, entry = 50255}, -- Dread Pirate Ring
     {class = 4, item = 12, entry = 42991}, -- Swift Hand of Justice
-    {class = 4, item = 13, entry = 42992}, -- Discerning Eye of the Beast
+    {class = 4, item = 13, entry = 44098}, -- Inherited Insignia of the Alliance
 
-    -- Rogue (8)
-    {class = 8, item =  1, entry = 42952}, -- Stained Shadowcraft Shoulders
-    {class = 8, item =  2, entry = 48689}, -- Stained Shadowcraft Tunic
-    {class = 8, item =  3, entry =  2126}, -- Cracked Leather Pants
-    {class = 8, item =  4, entry =  2122}, -- Cracked Leather Belt
-    {class = 8, item =  5, entry =  2125}, -- Cracked Leather Gloves
-    {class = 8, item =  6, entry =  2123}, -- Cracked Leather Boots
-    {class = 8, item =  7, entry =  2124}, -- Cracked Leather Bracers
+    -- Priest (5)
+    {class = 5, item =  1, entry = 42985}, -- Tattered Dreadmist Mantle
+    {class = 5, item =  2, entry = 48691}, -- Tattered Dreadmist Robe
+    {class = 5, item =  3, entry =  2120}, -- Thin Cloth Pants
+    {class = 5, item =  4, entry =  3599}, -- Thin Cloth Belt
+    {class = 5, item =  5, entry =  2119}, -- Thin Cloth Gloves
+    {class = 5, item =  6, entry =  2117}, -- Thin Cloth Shoes
+    {class = 5, item =  7, entry =  3600}, -- Thin Cloth Bracers
+    {class = 5, item =  8, entry = 14385}, -- Durability Cloak
+    {class = 5, item =  9, entry = 42947}, -- Dignified Headmaster's Charg5    {class = 5, item = 10, entry = 27403}, -- Stillpine Stinger
+    {class = 5, item = 11, entry = 50255}, -- Dread Pirate Ring
+    {class = 5, item = 12, entry = 42991}, -- Swift Hand of Justice
+    {class = 5, item = 13, entry = 42992}, -- Discerning Eye of the Beast
+
+    -- Shaman (7)
+    {class = 7, item =  1, entry = 42951}, -- Mystical Pauldrons of the Elements
+    {class = 7, item =  2, entry = 48683}, -- Mystical Vest of the Elements
+    {class = 7, item =  3, entry =  2126}, -- Cracked Leather Pants
+    {class = 7, item =  4, entry =  2122}, -- Cracked Leather Belt
+    {class = 7, item =  5, entry =  2125}, -- Cracked Leather Gloves
+    {class = 7, item =  6, entry =  2123}, -- Cracked Leather Boots
+    {class = 7, item =  7, entry =  2124}, -- Cracked Leather Bracers
+    {class = 7, item =  8, entry = 14385}, -- Durability Cloak
+    {class = 7, item =  9, entry = 48718}, -- Repurposed Lava Dredger
+    {class = 7, item = 10, entry = 42948}, -- Devout Aurastone Hammer
+    {class = 7, item = 11, entry = 50255}, -- Dread Pirate Ring
+    {class = 7, item = 12, entry = 42991}, -- Swift Hand of Justice
+    {class = 7, item = 13, entry = 42992}, -- Discerning Eye of the Beast
+
+    -- Mage (8)
+    {class = 8, item =  1, entry = 42985}, -- Tattered Dreadmist Mantle
+    {class = 8, item =  2, entry = 48691}, -- Tattered Dreadmist Robe
+    {class = 8, item =  3, entry =  2120}, -- Thin Cloth Pants
+    {class = 8, item =  4, entry =  3599}, -- Thin Cloth Belt
+    {class = 8, item =  5, entry =  2119}, -- Thin Cloth Gloves
+    {class = 8, item =  6, entry =  2117}, -- Thin Cloth Shoes
+    {class = 8, item =  7, entry =  3600}, -- Thin Cloth Bracers
     {class = 8, item =  8, entry = 14385}, -- Durability Cloak
-    {class = 8, item =  9, entry = 42944}, -- Balanced Heartseeker
-    {class = 8, item = 10, entry = 42944}, -- Balanced Heartseeker
+    {class = 8, item =  9, entry = 42947}, -- Dignified Headmaster's Charge
+    {class = 8, item = 10, entry = 27403}, -- Stillpine Stinger
     {class = 8, item = 11, entry = 50255}, -- Dread Pirate Ring
     {class = 8, item = 12, entry = 42991}, -- Swift Hand of Justice
-    {class = 8, item = 13, entry = 44098}, -- Inherited Insignia of the Alliance
+    {class = 8, item = 13, entry = 42992}, -- Discerning Eye of the Beast
 
-    -- Priest (16)
-    {class = 16, item =  1, entry = 42985}, -- Tattered Dreadmist Mantle
-    {class = 16, item =  2, entry = 48691}, -- Tattered Dreadmist Robe
-    {class = 16, item =  3, entry =  2120}, -- Thin Cloth Pants
-    {class = 16, item =  4, entry =  3599}, -- Thin Cloth Belt
-    {class = 16, item =  5, entry =  2119}, -- Thin Cloth Gloves
-    {class = 16, item =  6, entry =  2117}, -- Thin Cloth Shoes
-    {class = 16, item =  7, entry =  3600}, -- Thin Cloth Bracers
-    {class = 16, item =  8, entry = 14385}, -- Durability Cloak
-    {class = 16, item =  9, entry = 42947}, -- Dignified Headmaster's Charge
-    {class = 16, item = 10, entry = 27403}, -- Stillpine Stinger
-    {class = 16, item = 11, entry = 50255}, -- Dread Pirate Ring
-    {class = 16, item = 12, entry = 42991}, -- Swift Hand of Justice
-    {class = 16, item = 13, entry = 42992}, -- Discerning Eye of the Beast
+    -- Warlock (9)
+    {class = 9, item =  1, entry = 42985}, -- Tattered Dreadmist Mantle
+    {class = 9, item =  2, entry = 48691}, -- Tattered Dreadmist Robe
+    {class = 9, item =  3, entry =  2120}, -- Thin Cloth Pants
+    {class = 9, item =  4, entry =  3599}, -- Thin Cloth Belt
+    {class = 9, item =  5, entry =  2119}, -- Thin Cloth Gloves
+    {class = 9, item =  6, entry =  2117}, -- Thin Cloth Shoes
+    {class = 9, item =  7, entry =  3600}, -- Thin Cloth Bracers
+    {class = 9, item =  8, entry = 14385}, -- Durability Cloak
+    {class = 9, item =  9, entry = 42947}, -- Dignified Headmaster's Charge
+    {class = 9, item = 10, entry = 27403}, -- Stillpine Stinger
+    {class = 9, item = 11, entry = 50255}, -- Dread Pirate Ring
+    {class = 9, item = 12, entry = 42991}, -- Swift Hand of Justice
+    {class = 9, item = 13, entry = 42992}, -- Discerning Eye of the Beast
 
-    -- Shaman (64)
-    {class = 64, item =  1, entry = 42951}, -- Mystical Pauldrons of the Elements
-    {class = 64, item =  2, entry = 48683}, -- Mystical Vest of the Elements
-    {class = 64, item =  3, entry =  2126}, -- Cracked Leather Pants
-    {class = 64, item =  4, entry =  2122}, -- Cracked Leather Belt
-    {class = 64, item =  5, entry =  2125}, -- Cracked Leather Gloves
-    {class = 64, item =  6, entry =  2123}, -- Cracked Leather Boots
-    {class = 64, item =  7, entry =  2124}, -- Cracked Leather Bracers
-    {class = 64, item =  8, entry = 14385}, -- Durability Cloak
-    {class = 64, item =  9, entry = 48718}, -- Repurposed Lava Dredger
-    {class = 64, item = 10, entry = 42948}, -- Devout Aurastone Hammer
-    {class = 64, item = 11, entry = 50255}, -- Dread Pirate Ring
-    {class = 64, item = 12, entry = 42991}, -- Swift Hand of Justice
-    {class = 64, item = 13, entry = 42992}, -- Discerning Eye of the Beast
-
-    -- Mage (128)
-    {class = 128, item =  1, entry = 42985}, -- Tattered Dreadmist Mantle
-    {class = 128, item =  2, entry = 48691}, -- Tattered Dreadmist Robe
-    {class = 128, item =  3, entry =  2120}, -- Thin Cloth Pants
-    {class = 128, item =  4, entry =  3599}, -- Thin Cloth Belt
-    {class = 128, item =  5, entry =  2119}, -- Thin Cloth Gloves
-    {class = 128, item =  6, entry =  2117}, -- Thin Cloth Shoes
-    {class = 128, item =  7, entry =  3600}, -- Thin Cloth Bracers
-    {class = 128, item =  8, entry = 14385}, -- Durability Cloak
-    {class = 128, item =  9, entry = 42947}, -- Dignified Headmaster's Charge
-    {class = 128, item = 10, entry = 27403}, -- Stillpine Stinger
-    {class = 128, item = 11, entry = 50255}, -- Dread Pirate Ring
-    {class = 128, item = 12, entry = 42991}, -- Swift Hand of Justice
-    {class = 128, item = 13, entry = 42992}, -- Discerning Eye of the Beast
-
-    -- Warlock (256)
-    {class = 256, item =  1, entry = 42985}, -- Tattered Dreadmist Mantle
-    {class = 256, item =  2, entry = 48691}, -- Tattered Dreadmist Robe
-    {class = 256, item =  3, entry =  2120}, -- Thin Cloth Pants
-    {class = 256, item =  4, entry =  3599}, -- Thin Cloth Belt
-    {class = 256, item =  5, entry =  2119}, -- Thin Cloth Gloves
-    {class = 256, item =  6, entry =  2117}, -- Thin Cloth Shoes
-    {class = 256, item =  7, entry =  3600}, -- Thin Cloth Bracers
-    {class = 256, item =  8, entry = 14385}, -- Durability Cloak
-    {class = 256, item =  9, entry = 42947}, -- Dignified Headmaster's Charge
-    {class = 256, item = 10, entry = 27403}, -- Stillpine Stinger
-    {class = 256, item = 11, entry = 50255}, -- Dread Pirate Ring
-    {class = 256, item = 12, entry = 42991}, -- Swift Hand of Justice
-    {class = 256, item = 13, entry = 42992}, -- Discerning Eye of the Beast
-
-    -- Druid (1024)
-    {class = 1024, item =  1, entry = 42984}, -- Preened Ironfeather Shoulders
-    {class = 1024, item =  2, entry = 48687}, -- Preened Ironfeather Breastplate
-    {class = 1024, item =  3, entry =  2126}, -- Cracked Leather Pants
-    {class = 1024, item =  4, entry =  2122}, -- Cracked Leather Belt
-    {class = 1024, item =  5, entry =  2125}, -- Cracked Leather Gloves
-    {class = 1024, item =  6, entry =  2123}, -- Cracked Leather Boots
-    {class = 1024, item =  7, entry =  2124}, -- Cracked Leather Bracers
-    {class = 1024, item =  8, entry = 14385}, -- Durability Cloak
-    {class = 1024, item =  9, entry = 48716}, -- Venerable Mass of McGowan
-    {class = 1024, item = 10, entry = 42947}, -- Dignified Headmaster's Charge
-    {class = 1024, item = 11, entry = 50255}, -- Dread Pirate Ring
-    {class = 1024, item = 12, entry = 42991}, -- Swift Hand of Justice
-    {class = 1024, item = 13, entry = 42992}, -- Discerning Eye of the Beast
+    -- Druid (11)
+    {class = 11, item =  1, entry = 42984}, -- Preened Ironfeather Shoulders
+    {class = 11, item =  2, entry = 48687}, -- Preened Ironfeather Breastplate
+    {class = 11, item =  3, entry =  2126}, -- Cracked Leather Pants
+    {class = 11, item =  4, entry =  2122}, -- Cracked Leather Belt
+    {class = 11, item =  5, entry =  2125}, -- Cracked Leather Gloves
+    {class = 11, item =  6, entry =  2123}, -- Cracked Leather Boots
+    {class = 11, item =  7, entry =  2124}, -- Cracked Leather Bracers
+    {class = 11, item =  8, entry = 14385}, -- Durability Cloak
+    {class = 11, item =  9, entry = 48716}, -- Venerable Mass of McGowan
+    {class = 11, item = 10, entry = 42947}, -- Dignified Headmaster's Charge
+    {class = 11, item = 11, entry = 50255}, -- Dread Pirate Ring
+    {class = 11, item = 12, entry = 42991}, -- Swift Hand of Justice
+    {class = 11, item = 13, entry = 42992}, -- Discerning Eye of the Beast
 
 }
 
-function findGear(class, item)
+local function findGear(class, item)
     for i, v in ipairs(Gear) do
         if v.class==class and v.item==item then
             return v.entry
@@ -210,7 +219,7 @@ function findGear(class, item)
     end
 end
 
-function grantSkills(player, class)
+local function grantSkills(player, class)
     print("["..MODULE_NAME.."]: Grant Skills to "..player:GetName())
     for i, v in ipairs(Skills) do
         if v.class==class or v.class==0 then
@@ -220,14 +229,37 @@ function grantSkills(player, class)
     end
 end
 
-function firstLogin(event, player)
-    class = player:GetClassMask()
+local function grantMounts(player, race)
+    print("["..MODULE_NAME.."]: Grant Mounts to "..player:GetName())
+
+    -- Riding Skills
+    player:CastSpell(player, 33389, true)
+    player:CastSpell(player, 33392, true)
+    player:CastSpell(player, 34092, true)
+    player:CastSpell(player, 34093, true)
+    player:LearnSpell(54197)
+
+    -- Mounts
+    for i, v in ipairs(Mounts) do
+        if v.race==race or v.race==0 then
+            player:LearnSpell(v.entry)
+            print("["..MODULE_NAME.."]: "..player:GetName().." learned "..v.entry.." (mount)")
+        end
+    end
+
+end
+
+local function firstLogin(event, player)
+    class = player:GetClass()
     level = player:GetLevel()
+    race = player:GetRace()
     if level > 1 then
         return
     end
     print("["..MODULE_NAME.."]: "..player:GetName().." created by "..player:GetAccountName())
+    player:SetKnownTitle(1)
     grantSkills(player, class)
+    grantMounts(player, race)
     emailTo = player:GetGUIDLow()
     emailStationery = 61
     emailFrom = 0
