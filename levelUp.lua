09ad9d8ae11e88ae48e9be1642811cb1e25478a5
Michael Bird <michaelbirdtx@protonmail.com>
2022-07-07 21:30:33 -0500

    Bump version.

--- levelUp.lua
+++ levelUp.lua
@@ -1,5 +1,5 @@
 local MODULE_NAME = "Eluna levelUp"
-local MODULE_VERSION = '1.6'
+local MODULE_VERSION = '1.6.1'
 local MODULE_AUTHOR = "Mpromptu Gaming"
 
 print("["..MODULE_NAME.."]: Loaded, Version "..MODULE_VERSION.." Active")
