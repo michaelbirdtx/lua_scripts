8e0c00ea46a5f84caf3fc55a2b5fd7c9c0b89cf7
Michael Bird <michaelbirdtx@protonmail.com>
2022-07-14 06:02:04 -0500

    Fixed flag being removed.

--- initServer.lua
+++ initServer.lua
@@ -5,7 +5,6 @@ local MODULE_AUTHOR = "Mpromptu Gaming"
 print("["..MODULE_NAME.."]: Loaded, Version "..MODULE_VERSION.." Active")
 
 local function onZoneUpdate(event, player, newZone, newArea)
-    player:RemoveFlag(150, 32)
     if newZone == 616 or newZone == 876 then
         player:SetFlag(150, 32)
     end
