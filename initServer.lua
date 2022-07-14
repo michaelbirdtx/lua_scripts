da384cf2f3c08c45cc89cd8239dfba90c3d57a5a
Michael Bird <michaelbirdtx@protonmail.com>
2022-07-14 05:55:36 -0500

    Removed debug statements.

--- initServer.lua
+++ initServer.lua
@@ -6,10 +6,8 @@ print("["..MODULE_NAME.."]: Loaded, Version "..MODULE_VERSION.." Active")
 
 local function onZoneUpdate(event, player, newZone, newArea)
     player:RemoveFlag(150, 32)
-    print("["..MODULE_NAME.."]: Player: "..player:GetName().." New Zone: "..newZone.." Flags: "..player:GetInt32Value(150))
     if newZone == 616 or newZone == 876 then
         player:SetFlag(150, 32)
-        print(player:GetName().." has arrived at the Guild House.")
     end
 end
 
