788fdeb88c563aa37531b1b43b883a6fdc073dd0
Michael Bird <michaelbirdtx@protonmail.com>
2022-07-15 11:03:06 -0500

    Continued development.

--- hirelings.lua
+++ hirelings.lua
@@ -4,44 +4,76 @@ local MODULE_AUTHOR = "Mpromptu Gaming"
 
 print("["..MODULE_NAME.."]: Loaded, Version "..MODULE_VERSION.." Active")
 
+local Spells = {
+    [669001] = 335, -- Taunt
+}
+
 local function onChatMessage(event, player, msg, _, lang)
-    if (msg:find('#hire') == 1) then
-        --local pet = PerformIngameSpawn(1, sender, unit:GetMapId(), unit:GetInstanceId(), unit:GetX(), unit:GetY(), unit:GetZ(), unit:GetO(), false, 5000)
+    if (msg:find('#hire sword') == 1) then
         hLevel = player:GetLevel()+math.random(1,3)
         hHealth = (player:GetMaxHealth()/player:GetLevel()) * hLevel
-        hireling = PerformIngameSpawn(1, 669001, player:GetMapId(), player:GetInstanceId(), player:GetX(), player:GetY(), player:GetZ(), player:GetO(), false, 2400000)
-        print("Player Level: "..player:GetLevel())
-        print("Player Max Health: "..player:GetMaxHealth())
-        print("Player Min Damage: "..player:GetFloatValue(70))
-        print("Player Max Damage: "..player:GetFloatValue(71))
-        print("Hireling Starting Level: "..hireling:GetLevel())
+        hireling = PerformIngameSpawn(1, 669001, player:GetMapId(), player:GetInstanceId(), player:GetX(), player:GetY(), player:GetZ(), player:GetO(), false, 600000)
         hireling:SetFaction(35)
         hireling:SetCreatorGUID(player:GetGUID())
         hireling:SetOwnerGUID(player:GetGUID())
         hireling:SetLevel(hLevel)
         hireling:SetMaxHealth(hHealth)
         hireling:SetHealth(hHealth)
-        hireling:SetFloatValue(70, player:GetFloatValue(70)*1.5)
-        hireling:SetFloatValue(71, player:GetFloatValue(71)*1.5)
-        hireling:SetDefaultMovementType(14)
+        hireling:SetFloatValue(70, player:GetFloatValue(70)*1.5) -- Set MinDamage based on player
+        hireling:SetFloatValue(71, player:GetFloatValue(71)*1.5) -- Set MaxDamage based on player
+        --hireling:SetDefaultMovementType(14)
         hireling:MoveFollow(player, 3, 60)
         hireling:SetEquipmentSlots(11786, 0, 0)
-        print("Hireling Matched Level: "..hireling:GetLevel())
-        print("Hireling Max Health: "..hireling:GetMaxHealth())
-        print("Hireling Min Damage: "..hireling:GetFloatValue(70))
-        print("Hireling Max Damage: "..hireling:GetFloatValue(71))
     end
 end
 
 local function onEnterCombat(event, creature, target)
-    creature:CastSpell(target, 355, true)
+    spell = Spells[creature:GetEntry()]
+    print("Hireling casts "..spell)
+    creature:CastSpell(target, spell, true)
 end
 
 local function onLeaveCombat(event, creature)
+    player = creature:GetOwner()
     creature:SetHealth(creature:GetMaxHealth())
     creature:SetSheath(0)
+    creature:MoveFollow(player, 3, 60)
+end
+
+local function onRemove(event, creature)
+    player = creature:GetOwner()
+    creature:SendUnitSay("Fair well, "..player:GetName()..".", 0)
+    creature:PerformEmote(3)
+end
+
+local function hirelingOnHello(event, player, unit)
+    player:GossipSetText("Greetings, "..player:GetClassAsString()..".\n\nWhat can I do for you?")
+    player:GossipMenuAddItem(0, "Follow me, there's killing to be done.", 0, 1)
+    player:GossipMenuAddItem(0, "Wait here, I'll bring them to you.", 0, 2)
+    player:GossipMenuAddItem(0, "You have completed your work here. I release you from your contract.", 0, 3)
+    player:GossipSendMenu(0x7FFFFFFF, unit)
+end
+
+function hirelingOnSelect(event, player, unit, sender, intid, code)
+    if intid == 1 then
+        unit:MoveExpire()
+        unit:MoveIdle()
+        unit:MoveFollow(player, 3, 60)
+        player:GossipComplete()
+    end
+    if intid == 2 then
+        unit:MoveExpire()
+        unit:MoveIdle()
+        player:GossipComplete()
+    end
+    if intid == 3 then
+        unit:DespawnOrUnsummon(0)
+    end
 end
 
 RegisterPlayerEvent(18, onChatMessage)
 RegisterCreatureEvent(669001, 1, onEnterCombat)
 RegisterCreatureEvent(669001, 2, onLeaveCombat)
+RegisterCreatureEvent(669001, 37, onRemove)
+RegisterCreatureGossipEvent(669001, 1, hirelingOnHello)
+RegisterCreatureGossipEvent(669001, 2, hirelingOnSelect)
---
+++ sql/npcs/hireling_sellsword.sql
@@ -0,0 +1,19 @@
+SET
+@Entry 			:= 669001,
+@Model1 		:= 17214,
+@Model2 		:= 3054,
+@Model3 		:= 1314,
+@Model4 		:= 17671,
+@Name 			:= 'Sellsword',
+@MinLevel 		:= 10,
+@MaxLevel 		:= 10,
+@Faction 		:= 35,
+@DetectionRange := 50,
+@AIName			:= "CombatAI"
+;
+
+DELETE FROM acore_world.creature_template WHERE entry = @Entry;
+INSERT INTO acore_world.creature_template
+	(`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,`KillCredit1`,`KillCredit2`,`modelid1`,`modelid2`,`modelid3`,`modelid4`,`name`,`subname`,`IconName`,`gossip_menu_id`,`minlevel`,`maxlevel`,`exp`,`faction`,`npcflag`,`speed_walk`,`speed_run`,`speed_swim`,`speed_flight`,`detection_range`,`scale`,`rank`,`dmgschool`,`DamageModifier`,`BaseAttackTime`,`RangeAttackTime`,`BaseVariance`,`RangeVariance`,`unit_class`,`unit_flags`,`unit_flags2`,`dynamicflags`,`family`,`trainer_type`,`trainer_spell`,`trainer_class`,`trainer_race`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,`PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,`HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`ExperienceModifier`,`RacialLeader`,`movementId`,`RegenHealth`,`mechanic_immune_mask`,`spell_school_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`)
+	VALUES (@Entry,0,0,0,0,0,@Model1,@Model2,@Model3,@Model4,@Name,'','',0,@MinLevel,@MaxLevel,2,@Faction,1,1,1.14286,1,1,@DetectionRange,1,0,0,1,2000,2000,1,1,1,8,2048,0,0,0,0,0,0,7,0,0,0,0,0,0,0,0,@AIName,0,1,1.1,0.77,1,1,0,121,1,0,0,0,'',12340);
+SELECT * FROM acore_world.creature_template WHERE entry = @Entry;
