9d968c3ebdfb0568d11d04fd4a286906b0b5e6b1
Michael Bird <michaelbirdtx@protonmail.com>
2022-07-21 10:50:28 -0500

    Bump version.

--- dressMe.lua
+++ dressMe.lua
@@ -1,5 +1,5 @@
 local MODULE_NAME = "Eluna dressMe"
-local MODULE_VERSION = '1.3'
+local MODULE_VERSION = '1.3.1'
 local MODULE_AUTHOR = "Mpromptu Gaming"
 
 print("["..MODULE_NAME.."]: Loaded, Version "..MODULE_VERSION.." Active")
