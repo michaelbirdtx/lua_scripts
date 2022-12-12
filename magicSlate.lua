3f32904a4013b1e7d942846025875d80b1766183
Michael Bird <github@michaelbirdtx.com>
2022-12-11 21:23:43 -0600

    Added food delivery. Initial release.

--- magicSlate.lua
+++ magicSlate.lua
@@ -21,6 +21,16 @@ local Songs = {
     [12] = 15885,
 }
 
+local function randomSong()
+    local song = math.random(1,#Songs)
+    return song
+end
+
+local function randomDelay(maxMinutes)
+    delay = math.random(1,maxMinutes) * 60
+    return delay
+end
+
 local function onChatMessage(event, player, msg, _, lang)
     if (msg:find('#magicbank') == 1) and player:GetGMRank() > 0 then
         player:SendShowBank(player)
@@ -31,8 +41,7 @@ local function onChatMessage(event, player, msg, _, lang)
         return false
     end
     if (msg:find('#magicmusic') == 1) and player:GetGMRank() > 0 then
-        local randomSong = math.random(1,#Songs)
-        player:PlayDirectSound(Songs[randomSong], player)
+        player:PlayDirectSound(randomSong(), player)
         return false
     end
 end
@@ -43,6 +52,7 @@ local function onHelloMagicSlate(event, player, object)
     player:GossipMenuAddItem(6, "\"...access my bank.\"", 1, 1)
     player:GossipMenuAddItem(7, "\"...read my mail.\"", 1, 2)
     player:GossipMenuAddItem(4, "\"...listen to some music.\"", 1, 3)
+    player:GossipMenuAddItem(2, "\"...order some more of The Stuff!\"", 1, 4)
     player:GossipSendMenu(0x7FFFFFFF, player, SLATE_ENTRY)
 end
 
@@ -56,6 +66,20 @@ local function onSelectMagicSlate(event, player, object, sender, intid, code, me
         local randomSong = math.random(1,#Songs)
         player:PlayDirectSound(Songs[randomSong], player)
         player:SendBroadcastMessage("A song that only you can hear begins playing...")
+    elseif intid == 4 then
+        SendMail(
+            "Thank you for your order!",
+            "",
+            player:GetGUIDLow(),
+            0,
+            61,
+            randomDelay(3), -- Delay
+            0,
+            100, -- COD
+            23364,
+            1000
+        )
+        player:SendBroadcastMessage("Your order of The Stuff is being prepared, and will arrive in your mailbox in just a few minutes.")
     end
 end
 
