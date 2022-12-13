d300ce85a52a96bec90db638f121ea482f06a2be
Michael Bird <git@michaelbirdtx.com>
2022-12-13 15:56:12 +0000

    Disabled XP bonus for now.

--- hybridClasses.lua
+++ hybridClasses.lua
@@ -1,5 +1,5 @@
 local MODULE_NAME = "Eluna hybridClasses"
-local MODULE_VERSION = '1.0.1'
+local MODULE_VERSION = '1.0.2'
 local MODULE_AUTHOR = "Mpromptu Gaming"
 
 print("["..MODULE_NAME.."]: Loaded, Version "..MODULE_VERSION.." Active")
@@ -98,5 +98,5 @@ ENGINE=InnoDB;
 ]])
 
 RegisterPlayerEvent(4, onLogout)
-RegisterPlayerEvent(12, onGainXP)
+--RegisterPlayerEvent(12, onGainXP)
 RegisterPlayerEvent(13, onLevelUp)
